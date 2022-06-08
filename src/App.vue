367f5c611f6817063b4115bc9b80d446290d270f
xjb <1159205385@qq.com>
2022-06-08 16:22:48 +0800

    组件化静态页面

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="container">
+      <Add></Add>
+      <List></List>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Header from '@/components/Header'
+import Add from '@/components/Add'
+import List from '@/components/List'
+export default {
+  name: '',
+  components:{
+    Header,
+    Add,
+    List
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
